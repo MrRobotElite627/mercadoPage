c110ae345690968a02c607e504dfacfe2d1d5d6c
CodexPE Jairo <mrrobotelite627@gmail.com>
2024-09-07 14:16:21 -0500

    Resolución de conflictos

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { MercadoPagoConfig, Preference, Payment } from "mercadopago";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore"; // Importa getFirestore
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
